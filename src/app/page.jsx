63bac324badadc2e85c045d380b791d22d26f398
Olga1Chernova <olya53800@gmail.com>
2023-07-07 23:53:54 +0300

    changed footer and home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,23 @@
 import Image from 'next/image'
+import HeroImg from 'public/hero.png'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your idea into reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={HeroImg} alt="hero" className={styles.img} />
+      </div>
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
+import Image from 'next/image';
 
+import css from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={css.container}>
       <div>©2023 Posts App. All rights reserved.</div>
-      <div></div>
+      <div className={css.social}>
+        <Image src="/1.png" width={15} height={15} className={css.icon} alt="posts app facebook" />
+        <Image src="/2.png" width={15} height={15} className={css.icon} alt="posts app instagram" />
+        <Image src="/3.png" width={15} height={15} className={css.icon} alt="posts app twitter" />
+        <Image src="/4.png" width={15} height={15} className={css.icon} alt="posts app youtube" />
+      </div>
     </div>
   );
 }
